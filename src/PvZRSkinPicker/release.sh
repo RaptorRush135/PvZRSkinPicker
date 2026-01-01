cf91f3dda607f8a7d9c1a8bf926c7a0adb1223f8
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-01-01 13:53:22 -0600

    fix: dont disable debug symbols & set SourceRoot

--- src/PvZRSkinPicker/release.sh
+++ src/PvZRSkinPicker/release.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+dotnet clean -c Release
+
 dotnet build -c Release \
   -p:ContinuousIntegrationBuild=true \
   -p:Deterministic=true \
-  -p:DebugType=none \
-  -p:DebugSymbols=false
+  -v:diag
 
 read -p "Build finished. Press Enter to exit..."
