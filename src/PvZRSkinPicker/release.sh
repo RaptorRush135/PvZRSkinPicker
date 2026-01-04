ac194d2a2e7164717c810f1cc5e239aac77711e5
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-01-03 21:07:16 -0600

    build: zip output, set version as 1.0.0, set link, and more tweaks to build files

--- src/PvZRSkinPicker/release.sh
+++ src/PvZRSkinPicker/release.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+trap 'echo "Error occurred! Press Enter to exit..."; read' ERR
+
 dotnet clean -c Release
 
 dotnet build -c Release \
   -p:ContinuousIntegrationBuild=true \
   -p:Deterministic=true \
+  -p:DebugType=embedded \
   -v:diag
 
 read -p "Build finished. Press Enter to exit..."
