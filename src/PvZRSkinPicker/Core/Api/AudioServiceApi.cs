9b482caf6f0f643861dcbac347a71e35d43937cf
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-01-03 22:38:03 -0600

    fix: bypass audio debounce in 'PlayFoleyPitch' (to allow sound to be played when paused)

--- src/PvZRSkinPicker/Core/Api/AudioServiceApi.cs
+++ src/PvZRSkinPicker/Core/Api/AudioServiceApi.cs
@@ -1,11 +1,19 @@
 ﻿namespace PvZRSkinPicker.Api;
 
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+#pragma warning disable IDE0051 // Remove unused private member
+
 using System.Diagnostics.CodeAnalysis;
+
+using HarmonyLib;
+
+using Il2Cpp;
 
 using Il2CppReloaded.Services;
 
 using MelonLoader;
 
+[HarmonyPatch]
 internal static class AudioServiceApi
 {
     private static IAudioService? audioService;
@@ -18,6 +26,8 @@ internal static class AudioServiceApi
     {
         AppCoreApi.OnAudioServiceReady += value => audioService = value;
     }
+
+    private static bool BypassDebounce { get; set; }
 
     public static void Initialize() => GC.KeepAlive(audioService);
 
@@ -32,6 +42,26 @@ internal static class AudioServiceApi
         }
 
         float pitch = UnityEngine.Random.Range(-10, 10);
-        audioService.PlayFoleyPitch(foleyType, pitch);
+        try
+        {
+            BypassDebounce = true;
+            audioService.PlayFoleyPitch(foleyType, pitch);
+        }
+        finally
+        {
+            BypassDebounce = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(AudioSourceWrapper), nameof(AudioSourceWrapper.TimeSinceLastPlayed), MethodType.Getter)]
+    private static bool Prefix(ref float __result)
+    {
+        if (BypassDebounce)
+        {
+            __result = 1;
+            return false;
+        }
+
+        return true;
     }
 }
